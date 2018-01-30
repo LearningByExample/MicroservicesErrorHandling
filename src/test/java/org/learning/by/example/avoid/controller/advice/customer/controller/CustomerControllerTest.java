0b7bf60bd38a724eb547e1c1bf479343fba51003
Juan Medina <jamedina@gmail.com>
2018-01-30 09:25:27 +0000

    creating structure for the tests

--- src/test/java/org/learning/by/example/avoid/controller/advice/customer/controller/CustomerControllerTest.java
+++ src/test/java/org/learning/by/example/avoid/controller/advice/customer/controller/CustomerControllerTest.java
@@ -1,5 +1,29 @@
 package org.learning.by.example.avoid.controller.advice.customer.controller;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.learning.by.example.avoid.controller.advice.application.ApplicationConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+@SpringBootTest(classes = ApplicationConfig.class)
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
 public class CustomerControllerTest {
 
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void mockMvcShouldBeReady() {
+    assertThat(mockMvc, is(notNullValue()));
+  }
 }
